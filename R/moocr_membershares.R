09aa2f14e5257aa9a3ee7626ea8e0df88d378a89
ahdvnd <hadavand.a@gmail.com>
2017-10-17 11:32:23 -0400

    new function

--- R/moocr_membershares.R
+++ R/moocr_membershares.R
@@ -9,49 +9,49 @@ moocr_membershares <- function(groupby = "roles") {
                  dplyr::filter(!is.na(course_membership_role)) %>% 
                  dplyr::group_by(course_membership_role) %>% 
                  dplyr::summarise(Total=n()) %>% 
-                 dplyr::mutate(Share=Total/sum(Total)) %>% 
+                 dplyr::mutate(Share=round(Total/sum(Total),2)) %>% 
                  dplyr::arrange(desc(Total))
          } else if (groupby == "country") {
              temp %>%
                  dplyr::filter(!is.na(country_cd)) %>% 
                  dplyr::group_by(country_cd) %>% 
                  dplyr::summarise(Total=n()) %>% 
-                 dplyr::mutate(Share=Total/sum(Total)) %>% 
+                 dplyr::mutate(Share=round(Total/sum(Total),2)) %>% 
                  dplyr::arrange(desc(Total))
          } else if (groupby == "language") {
              temp %>%
                  dplyr::filter(!is.na(profile_language_cd)) %>% 
                  dplyr::group_by(profile_language_cd) %>% 
                  dplyr::summarise(Total=n()) %>% 
-                 dplyr::mutate(Share=Total/sum(Total)) %>% 
+                 dplyr::mutate(Share=round(Total/sum(Total),2)) %>% 
                  dplyr::arrange(desc(Total))
          } else if (groupby == "gender") {
              temp %>%
                  dplyr::filter(!is.na(reported_or_inferred_gender)) %>% 
                  dplyr::group_by(reported_or_inferred_gender) %>% 
                  dplyr::summarise(Total=n()) %>% 
-                 dplyr::mutate(Share=Total/sum(Total)) %>% 
+                 dplyr::mutate(Share=round(Total/sum(Total),2)) %>% 
                  dplyr::arrange(desc(Total))
          } else if (groupby == "empstatus") {
              temp %>%
                  dplyr::filter(!is.na(employment_status)) %>% 
                  dplyr::group_by(employment_status) %>% 
                  dplyr::summarise(Total=n()) %>% 
-                 dplyr::mutate(Share=Total/sum(Total)) %>% 
+                 dplyr::mutate(Share=round(Total/sum(Total),2)) %>% 
                  dplyr::arrange(desc(Total))
          } else if (groupby == "education") {
              temp %>%
                  dplyr::filter(!is.na(educational_attainment)) %>% 
                  dplyr::group_by(educational_attainment) %>% 
                  dplyr::summarise(Total=n()) %>% 
-                 dplyr::mutate(Share=Total/sum(Total)) %>% 
+                 dplyr::mutate(Share=round(Total/sum(Total),2)) %>% 
                  dplyr::arrange(desc(Total))
          } else if (groupby == "stustatus") {
              temp %>%
                  dplyr::filter(!is.na(student_status)) %>% 
                  dplyr::group_by(student_status) %>% 
                  dplyr::summarise(Total=n()) %>% 
-                 dplyr::mutate(Share=Total/sum(Total)) %>% 
+                 dplyr::mutate(Share=round(Total/sum(Total),2)) %>% 
                  dplyr::arrange(desc(Total))
          } else {
              message("Please enter a valid value for 'groupby' attribute. Possible values are roles, country, language, gender, empstatus, education, and stustatus")
--- R/moocr_progress.R
+++ R/moocr_progress.R
@@ -1,32 +1,23 @@
 
 moocr_progress <- function(){
-    
-    progress <- function(x, y){
+    proceedstatus <- readline("The following procedure to import your data may take a while since the table course_progress is likely to be large. Do you want to proceed? (y/n): ")
+    if (proceedstatus == "y") {
+        progress <- function(x, y){
+            
+            temp <- x %>% 
+                dplyr::filter(!is.na(course_progress_ts)) %>% 
+                dplyr::group_by(jhu_user_id) %>%
+                dplyr::filter(course_progress_ts == max(course_progress_ts)) %>% # only to record the last activity
+                dplyr::left_join(y, by = "course_item_id", `copy`=TRUE) %>% # so that we know the name of the course item
+                dplyr::filter(!is.na(course_item_name)) %>% 
+                dplyr::group_by(course_item_order) %>% 
+                dplyr::summarise(Total=n()) %>% 
+                dplyr::arrange(desc(Total)) %>% 
+                dplyr::mutate(Share=round(Total/sum(Total), 2))
+        }
         
-        temp <- x %>% 
-            dplyr::filter(!is.na(course_progress_ts)) %>% 
-            dplyr::group_by(jhu_user_id) %>%
-            dplyr::filter(course_progress_ts == max(course_progress_ts)) %>% # only to record the last activity
-            dplyr::left_join(y, by = "course_item_id", `copy`=TRUE) %>% # so that we know the name of the course item
-            dplyr::filter(!is.na(course_item_name))
-        
-        prop.table(table(temp$course_item_order))
+        last_activity <- purrr::map(1:numcourses, ~ progress(all_tables[["course_progress"]][[.x]], all_tables[["course_items"]][[.x]]))
+        names(last_activity) <- coursenames  
+        return(last_activity) 
     }
-    
-last_activity <- purrr::map(1:numcourses, ~ progress(all_tables[["course_progress"]][[.x]], all_tables[["course_items"]][[.x]]))
-names(last_activity) <- coursenames  
-return(last_activity)    
 }
-
-
-temp <- all_tables[["course_progress"]][[1]] %>% 
-    dplyr::filter(!is.na(course_progress_ts)) %>% 
-    dplyr::group_by(jhu_user_id) %>%
-    dplyr::filter(course_progress_ts == max(course_progress_ts)) %>% # only to record the last activity
-    dplyr::left_join(all_tables[["course_items"]][[1]], by = "course_item_id", `copy`=TRUE) %>% # so that we know the name of the course item
-    dplyr::filter(!is.na(course_item_name)) %>% 
-    dplyr::count(course_item_order)
-
-
-temp <- tbl_df(temp)
-prop.table(table(temp$course_item_order))
